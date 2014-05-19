b2532343f54dd67e177af8cf6af4eb87fff01c6e
James Mosier <jamesdmosier@gmail.com>
2014-05-19 15:06:03 -0400

    worked on contact page (google map and form). added form elements to framework.css

--- js/app.js
+++ js/app.js
@@ -3,20 +3,20 @@
 
 	$.fn.portfolioResize = function () {
 
-		$(document).ready(function() {
-			var $wrapperWidth = $(this).width();
+		$(window).load(function() {		
+			var $wrapperWidth = $(this).outerWidth();
     		if ($wrapperWidth < 480) {
 
 				$('.posts-thumbs li a img').css({
-			        width:($("[data-id='post-thumbs']").width() / 1)
+			        width:($("[data-id='post-thumbs']").outerWidth() / 1)
 			    });
 			} else if ($wrapperWidth < 768 && $wrapperWidth > 480) { 
 				$('.posts-thumbs li a img').css({
-			        width:($("[data-id='post-thumbs']").width() / 2)
+			        width:($("[data-id='post-thumbs']").outerWidth() / 2)
 			    });
 			} else if ($wrapperWidth > 768) {
-			    $('.posts-thumbs li a img').css({
-			        width:($("[data-id='post-thumbs']").width() / 3) 
+			    $('.posts-thumbs li a img').css({			    	
+			        width:($("[data-id='post-thumbs']").outerWidth() / 3) 
 			    });
 			}
 		});
