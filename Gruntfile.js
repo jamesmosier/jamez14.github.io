1f16ed31295ed8569fa63609a037fc8e4387d09e
James Mosier <james.mosier@veritix.com>
2014-09-23 10:31:08 -0400

    added production grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,93 +1,89 @@
+var mozjpeg = require('imagemin-mozjpeg');
+
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        jekyll: {
-            build: {
-                dest: '_site'
-            }
-        },
+    jekyll: {
+      build: {
+        dest: '_site'
+      }
+    },
 
-        sass: {
-            dist: {
-                files: {
-                    'css/app.css': 'sass/app.scss'
-                }
-            }
-        },
-
-        watch: {
-            sass: {
-                files: 'sass/**/*.scss',
-                tasks: ['sass', 'cssmin']
-            },
-            jekyll: {
-                files: ['_layouts/*.html', '_includes/*.html', 'js/*.js', '_plugins/*', '_posts/*', 'css/app.css', 'about/*', 'contact/*', 'portfolio/*', 'index.html'],
-                tasks: ['jekyll']
-            },
-            scripts: {
-                files: ['js/*.js'],
-                tasks: ['uglify']
-            }
-        },
-
-        browser_sync: {
-            files: {
-                src: ['_site/css/*.css']
-            },
-            options: {
-                watchTask: true,
-                ghostMode: {
-                    clicks: true,
-                    scroll: true,
-                    links: true,
-                    forms: true
-                },
-                server: {
-                    baseDir: '_site'
-                }
-            }
-        },
-
-        cssmin: {
-            combine: {
-                files: {
-                    '_site/css/app.min.css': ['_site/css/app.css']
-                }
-            }
-        },
-        
-        uglify: {
-            my_target: {
-                files: {
-                    '_site/js/app.min.js': ['_site/lib/modernizr.js', '_site/js/app.js'],
-                    '_site/js/lib.min.js': ['_site/lib/jquery.typer.js', '_site/lib/jquery.hoverdir.js', '_site/lib/headroom/headroom.min.js']
-                }
-            }
+    sass: {
+      dist: {
+        files: {
+          'css/app.css': 'sass/app.scss'
         }
+      }
+    },
 
-        // uncss: {
-        //   dist: {
-        //     files: {
-        //       '_site/css/tidy.css': ['_site/index.html', '_site/about/index.html', '_site/portfolio/index.html', '_site/contact/index.html']
-        //     }
-        //   }
-        // }
+    watch: {
+      sass: {
+        files: 'sass/**/*.scss',
+        tasks: ['sass', 'cssmin']
+      },
+      jekyll: {
+        files: ['_layouts/*.html', '_includes/*.html', 'js/*.js', '_plugins/*', '_posts/*', 'css/app.css', 'about/*', 'contact/*', 'portfolio/*', 'index.html'],
+        tasks: ['jekyll']
+      },
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['uglify']
+      }
+    },
 
-    });
+    browser_sync: {
+      files: {
+        src: ['_site/css/*.css']
+      },
+      options: {
+        watchTask: true,
+        ghostMode: {
+          clicks: true,
+          scroll: true,
+          links: true,
+          forms: true
+        },
+        server: {
+          baseDir: '_site'
+        }
+      }
+    },
 
-    // Load the plugins
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-jekyll');
-    grunt.loadNpmTasks('grunt-browser-sync');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-uncss');
+    cssmin: {
+      combine: {
+        files: {
+          '_site/css/app.min.css': ['_site/css/app.css']
+        }
+      }
+    },
 
-    // Custom tasks
-    
-    grunt.registerTask('build', ['sass', 'jekyll']);
-    grunt.registerTask('postbuild', ['cssmin', 'uglify']);
-    grunt.registerTask('default', ['build', 'postbuild', 'browser_sync', 'watch']);
+    uglify: {
+      my_target: {
+        files: {
+          '_site/js/app.min.js': ['_site/lib/modernizr.js', '_site/js/app.js'],
+          '_site/js/lib.min.js': ['_site/lib/jquery.typer.js', '_site/lib/jquery.hoverdir.js', '_site/lib/headroom/headroom.min.js']
+        }
+      }
+    }
+
+
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+  // Custom tasks
+  grunt.registerTask('build', ['sass', 'jekyll']);
+  grunt.registerTask('postbuild', ['cssmin', 'uglify']);
+  grunt.registerTask('default', ['build', 'postbuild', 'browser_sync', 'watch']);
+
+  grunt.registerTask('prod', ['sass', 'jekyll', 'cssmin', 'uglify'])
 };
